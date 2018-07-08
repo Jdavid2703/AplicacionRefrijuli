4cd9895decef0f0f2eeef222b691627adfe92f8c
jpgomez024 <jpgomez024@misena.edu.co>
2018-07-07 22:14:18 -0500

    prueba

--- Refrijuli/src/vistas/ClienteController.java
+++ Refrijuli/src/vistas/ClienteController.java
@@ -21,7 +21,7 @@ public class ClienteController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODOnjaja
+        // TODOnjajajaj
     }    
     
 }
