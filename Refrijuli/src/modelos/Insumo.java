7c6cf7ced46089c52fade51441466d3fb7913932
carfecn <cagonzalez5549@misena.edu.co>
2018-07-08 23:22:27 -0500

    aja

--- Refrijuli/src/modelos/Insumo.java
+++ Refrijuli/src/modelos/Insumo.java
@@ -1,14 +1,280 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelos;
 
-/**
- *
- * @author Usuario
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
 public class Insumo {
-    
+
+    private IntegerProperty idInsumo;
+    private StringProperty nombre;
+    private StringProperty descripcion;
+    private IntegerProperty cantidad;
+    private Categoria idCategoria;
+    private IntegerProperty stockMin;
+    private IntegerProperty stockMax;
+    private Presentacion idPresentacion;
+    private Unidad idUnidad;
+    private IntegerProperty precioUnitario;
+    private IntegerProperty disponibilidad;
+
+    public Insumo(
+            Integer idInsumo,
+            String nombre,
+            String descripcion,
+            Integer cantidad,
+            Categoria idCategoria,
+            Integer stockMin,
+            Integer stockMax,
+            Presentacion idPresentacion,
+            Unidad idUnidad,
+            Integer precioUnitario,
+            Integer disponibilidad) {
+        this.idInsumo = new SimpleIntegerProperty(idInsumo);
+        this.nombre = new SimpleStringProperty(nombre);
+        this.descripcion = new SimpleStringProperty(descripcion);
+        this.cantidad = new SimpleIntegerProperty(cantidad);
+        this.idCategoria = idCategoria;
+        this.stockMin = new SimpleIntegerProperty(stockMin);
+        this.stockMax = new SimpleIntegerProperty(stockMax);
+        this.idPresentacion = idPresentacion;
+        this.idUnidad = idUnidad;
+        this.precioUnitario = new SimpleIntegerProperty(precioUnitario);
+        this.disponibilidad = new SimpleIntegerProperty(disponibilidad);
+    }
+
+    public Insumo() {
+    }
+
+    public IntegerProperty getIdInsumo() {
+        return idInsumo;
+    }
+
+    public void setIdInsumo(Integer idInsumo) {
+        this.idInsumo = new SimpleIntegerProperty(idInsumo);
+    }
+
+    public StringProperty getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = new SimpleStringProperty(nombre);
+    }
+
+    public StringProperty getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = new SimpleStringProperty(descripcion);
+    }
+
+    public IntegerProperty getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = new SimpleIntegerProperty(cantidad);
+    }
+
+    public Categoria getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Categoria idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public IntegerProperty getStockMin() {
+        return stockMin;
+    }
+
+    public void setStockMin(Integer stockMin) {
+        this.stockMin = new SimpleIntegerProperty(stockMin);
+    }
+
+    public IntegerProperty getStockMax() {
+        return stockMax;
+    }
+
+    public void setStockMax(Integer stockMax) {
+        this.stockMax = new SimpleIntegerProperty(stockMax);
+    }
+
+    public Presentacion getIdPresentacion() {
+        return idPresentacion;
+    }
+
+    public void setIdPresentacion(Presentacion idPresentacion) {
+        this.idPresentacion = idPresentacion;
+    }
+
+    public Unidad getIdUnidad() {
+        return idUnidad;
+    }
+
+    public void setIdUnidad(Unidad idUnidad) {
+        this.idUnidad = idUnidad;
+    }
+
+    public IntegerProperty getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(Integer precioUnitario) {
+        this.precioUnitario = new SimpleIntegerProperty(precioUnitario);
+    }
+
+    public IntegerProperty getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(Integer disponibilidad) {
+        this.disponibilidad = new SimpleIntegerProperty(disponibilidad);
+    }
+
+    //METODO LLENAR INFORMACION
+    public static void llenarInformacionInsumo(Connection connection,
+            ObservableList<Insumo> listaInsumo) {
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultado = statement.executeQuery(
+                    "SELECT idInsumo, "
+                    + "nombre,"
+                    + "descripcion,"
+                    + "cantidad,"
+                    + "idCategoria,"
+                    + "stockMin,"
+                    + "stockMax,"
+                    + "idPresentacion,"
+                    + "idUnidad,"
+                    + "precioUnitario,"
+                    + "disponibilidad"
+            );
+
+
+            while (resultado.next()) {
+                listaInsumo.add(
+                        new Insumo(
+                                resultado.getInt("idInsumo"),
+                                resultado.getString("nombre"),
+                                resultado.getString("descripcion"),
+                                resultado.getInt("cantidad"),
+                                new Categoria(resultado.getInt("idCategoria"),
+                                        resultado.getString("nombre"),
+                                        resultado.getString("descripcion")),
+                                resultado.getInt("stockMin"),
+                                resultado.getInt("stockMax"),
+                                new Presentacion(resultado.getInt("idPresentacion"),
+                                        resultado.getString("descripcion")),
+                                new Unidad(resultado.getInt("idUnidad"),
+                                        resultado.getString("tipoUnidad"),
+                                        resultado.getInt("cantidad")),
+                                resultado.getInt("precioUnitario"),
+                                resultado.getInt("disponibilidad")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //METODO GUARDAR 
+    public int guardarInsumo(Conexion conexion) {
+        try {
+            PreparedStatement ps = conexion.getConnection().prepareStatement(
+                    "INSERT INTO  Insumo ( "
+                    + "idInsumo, "
+                    + "nombre,"
+                    + "descripcion,"
+                    + "cantidad,"
+                    + "idCategoria,"
+                    + "stockMin,"
+                    + "stockMax,"
+                    + "idPresentacion,"
+                    + "idUnidad,"
+                    + "precioUnitario,"
+                    + "disponibilidad"
+                    + ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )"
+            );
+            ps.setInt(1, idInsumo.get());
+            ps.setString(2, nombre.get());
+            ps.setString(3, descripcion.get());
+            ps.setInt(4, cantidad.get());
+            ps.setInt(5, idCategoria.getIdCategoria());
+            ps.setInt(6, stockMin.get());
+            ps.setInt(7, stockMax.get());
+            ps.setInt(8, idPresentacion.getIdPresentacion());
+            ps.setInt(9, idUnidad.getIdUnidad());
+            ps.setInt(10, precioUnitario.get());
+            ps.setInt(11, disponibilidad.get());
+
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    //METODO ACTUALIZAR 
+
+    public int actualizarInsumo(Conexion conexion) {
+        try {
+            PreparedStatement ps = conexion.getConnection().prepareStatement(
+                    "UPDATE Insumo "
+                    + "SET idInsumo = ? ,"
+                    + "nombre = ?,"
+                    + "descripcion = ?,"
+                    + "cantidad = ?,"
+                    + "idCategoria = ?,"
+                    + "stockMin = ?,"
+                    + "stockMax = ?,"
+                    + "idPresentacion = ?,"
+                    + "idUnidad = ?,"
+                    + "precioUnitario = ?,"
+                    + "disponibilidad = ?"
+                    + "WHERE idInsumo = ?"
+            );
+
+            ps.setInt(1, idInsumo.get());
+            ps.setString(2, nombre.get());
+            ps.setString(3, descripcion.get());
+            ps.setInt(4, cantidad.get());
+            ps.setInt(6, idCategoria.getIdCategoria());
+            ps.setInt(7, stockMin.get());
+            ps.setInt(8, stockMax.get());
+            ps.setInt(9, idPresentacion.getIdPresentacion());
+            ps.setInt(10, idUnidad.getIdUnidad());
+            ps.setInt(11, precioUnitario.get());
+            ps.setInt(12, disponibilidad.get());
+
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+    //METODO ELIMINAR
+
+    public int eliminarInsumo(Conexion conexion) {
+        try {
+            PreparedStatement ps = conexion.getConnection().prepareStatement(
+                    "DELETE FROM insumo WHERE idInsumo = ?"
+            );
+            ps.setInt(1, idInsumo.get());
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
 }
--- Refrijuli/src/vistas/InsumoController.java
+++ Refrijuli/src/vistas/InsumoController.java
@@ -1,27 +1,189 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package vistas;
+package views;
+
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author Usuario
- */
+import model.Categoria;
+import model.Conexion;
+import model.Insumo;
+import model.Presentacion;
+import model.Unidad;
+
+
 public class InsumoController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
     
+     @FXML
+     private TextField txtIdInsumo;
+     @FXML
+     private TextField txtNombre;
+     @FXML
+     private TextArea txtDescripcion;
+     @FXML
+     private TextField txtCantidad;
+     @FXML 
+     private ComboBox<Categoria> cmbIdCategoria;
+     @FXML
+     private TextField txtStockMinimo;
+     @FXML
+     private TextField txtStockMaximo;
+     @FXML 
+     private ComboBox<Presentacion> cmbIdPresentacion;
+     @FXML 
+     private ComboBox<Unidad> cmbIdUnidad;
+     @FXML
+     private TextField txtPrecioUnidad;
+     @FXML
+     private TextField txtDisponibilidad;
+     @FXML
+     private Button btnNuevo; 
+     @FXML
+     private Button btnActualizar;
+     @FXML
+     private Button btnEliminar;
+     @FXML
+     private Button btnGuardar;
+
+     @FXML
+     private TableView<Insumo>tblViewInsumo;
+     @FXML
+     private TableColumn<Insumo, Number>clmnIdInsumo;
+     @FXML
+     private TableColumn<Insumo, String>clmnNombre;
+     @FXML
+     private TableColumn<Insumo, String>clmnDescripcion;
+     @FXML
+     private TableColumn<Insumo, Number>clmnCantidad;
+     @FXML
+     private TableColumn<Insumo, Categoria>clmnIdCategoria;
+     @FXML
+     private TableColumn<Insumo, Number>clmnStockMinimo;
+     @FXML
+     private TableColumn<Insumo, Number>clmnStockMaximo;
+     @FXML
+     private TableColumn<Insumo, Presentacion>clmnIdPresentacion;
+     @FXML
+     private TableColumn<Insumo, Unidad>clmnIdUnidad;
+     @FXML
+     private TableColumn<Insumo, Number>clmnPrecioUnidad;
+     @FXML
+     private TableColumn<Insumo, Number>clmnDisponibilidad;
+     
+     
+     @FXML
+    public void limpiarCamposNuevoRegistro() {
+        
+        txtIdInsumo.setText("null");
+        txtNombre.setText("null");
+        txtDescripcion.setText("null");
+        txtCantidad.setText("null");
+        cmbIdCategoria.setValue("null");
+        txtStockMinimo.setText("");
+        txtStockMaximo.setText("");
+        cmbIdPresentacion.setValue("null");
+        cmbIdUnidad.setValue("null");
+        txtPrecioUnidad.setText("null");
+        txtDisponibilidad.setText("null");
+        btnGuardar.setDisable(false);
+        btnActualizar.setDisable(true);
+        btnEliminar.setDisable(true);
+        txtIdInsumo.requestFocus();
+
+    }
+private Conexion conexion;
+private ObservableList<Insumo> listaInsumo;
+@FXML
+public void agregarInsumo() {
+        Insumo insumo = new Insumo(
+                Integer.valueOf(txtIdInsumo.getText()),
+                txtNombre.getText(),
+                txtDescripcion.getText(),
+                Integer.valueOf(txtCantidad.getText()),
+                cmbIdCategoria.getSelectionModel().getSelectedItem(),
+                Integer.valueOf(txtStockMinimo.getText()),
+                Integer.valueOf(txtStockMaximo.getText()),
+                cmbIdPresentacion.getSelectionModel().getSelectedItem(),
+                cmbIdUnidad.getSelectionModel().getSelectedItem(),
+                 Integer.valueOf(txtPrecioUnidad.getText()),
+                Integer.valueOf(txtPrecioUnidad.getText()));
+               
+        conexion.establecerConexion();
+        int resultado = insumo.guardarInsumo(conexion);
+        conexion.cerrarConexion();
+        
+        if (resultado == 1) {
+            listaInsumo.add(insumo);
 }
+}
+@FXML
+ public void actualizarInsumo() {
+        Insumo insumo = new Insumo(
+                Integer.valueOf(txtIdInsumo.getText()),
+                txtNombre.getText(),
+                txtDescripcion.getText(),
+                Integer.valueOf(txtCantidad.getText()),
+                cmbIdCategoria.getSelectionModel().getSelectedItem(),
+                Integer.valueOf(txtStockMinimo.getText()),
+                Integer.valueOf(txtStockMaximo.getText()),
+                cmbIdPresentacion.getSelectionModel().getSelectedItem(),
+                cmbIdUnidad.getSelectionModel().getSelectedItem(),
+                Integer.valueOf(txtPrecioUnidad.getText()),
+                Integer.valueOf(txtPrecioUnidad.getText()));
+        
+                
+       
+        conexion.establecerConexion();
+        int resultado = insumo.actualizarInsumo(conexion);
+        conexion.cerrarConexion();
+        
+        if (resultado == 1) {
+            listaInsumo.set(
+                    tblViewInsumo.getSelectionModel().getSelectedIndex(),
+                    insumo      
+            );
+        }
+ }
+ public void eliminarInsumo() {
+        Alert cuadroDialogoConfirmacion = new Alert(Alert.AlertType.CONFIRMATION);
+        cuadroDialogoConfirmacion.setTitle("Confirmacion");
+        cuadroDialogoConfirmacion.setHeaderText("Eliminar Registro");
+        cuadroDialogoConfirmacion.setContentText("¿Está Seguro de Eliminar el Registro?");
+        Optional<ButtonType> resultado = cuadroDialogoConfirmacion.showAndWait();
+        if (resultado.get() == ButtonType.OK) {
+            Insumo insumo = new Insumo();
+            insumo.setIdInsumo(Integer.valueOf(txtIdInsumo.getText()));
+            conexion.establecerConexion();
+            int r = insumo.eliminarInsumo(conexion);
+            conexion.cerrarConexion();
+            
+            if (r == 1) {
+                listaInsumo.remove(tblViewInsumo.getSelectionModel().getSelectedIndex());
+                Alert cuadroDialogo = new Alert(Alert.AlertType.INFORMATION);
+                cuadroDialogo.setContentText("Registro Eliminado con Éxito");
+                cuadroDialogo.setTitle("Registro Eliminado");
+                cuadroDialogo.setHeaderText("Resultado: ");
+                cuadroDialogo.showAndWait();
+            }
+            
+        }
+        
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     
+}            
